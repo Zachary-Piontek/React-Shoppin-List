e0cfd370144d3fe55170d8035fef48e525bd6059
Zachary Piontek <zakpiontek@gmail.com>
2022-09-29 10:19:23 -0500

    added user context

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,1 +1,14 @@
 // TODO -- create the user provider!
+import { useState, createContext } from 'react';
+import { getUser } from '../services/auth';
+
+const UserContext = createContext();
+
+const UserProvider = ({ child }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={{ user, setUser }}>{child}</UserContext.Provider>;
+};
+
+export { UserProvider, UserContext };
